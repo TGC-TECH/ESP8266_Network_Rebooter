7c34cae19b2b64cfb77ae81cd5b10cdf12758ed8
Toby Cowles <tobycowles1015@gmail.com>
2019-11-11 20:25:32 -0500

    Add Server Side Script

---
+++ launchCheck.sh
@@ -0,0 +1,43 @@
+while [ 2 > 1 ]
+do
+
+if ping -c 1 www.google.com &> /dev/null
+then
+  echo 1
+  echo "network OK"
+  Status=1
+  curl IPHERE/buzzOff #this command resets the buzzer
+
+else
+if [ Status < 2 ]
+  echo 0
+  echo "reseting network"
+  curl IPHERE/ledOff #powerOFF 
+  sleep 10s 
+  curl IPHERE/ledOn #powerON
+  Status=0
+  sleep 5m
+  fi
+fi
+
+if [ Status == 0 ]
+then 
+if ping -c 1 www.google.com &> /dev/null
+then
+  echo 1
+  echo "network Recovered"
+  mail -s "Network Recovered" email@address.com <<< "There was an event that caused the network to go down 5 minutes before this email was sent. We have successfully recovered"
+
+  Status=1
+
+else
+  echo 0
+  echo "Alerting User!"
+  curl IPHERE/buzzon #enables buzzer
+  Status=2
+  sleep 5m
+fi
+fi
+echo "sleeping"
+sleep 2m 
+done
