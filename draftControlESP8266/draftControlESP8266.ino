e4620c5e90392dfb8c7736029a59c1886fbd3452
Toby Cowles <tobycowles1015@gmail.com>
2019-11-11 20:27:11 -0500

    Add Arduino Code

---
+++ draftControlESP8266/draftControlESP8266.ino
@@ -0,0 +1,123 @@
+ /*
+ * ESP8266 NodeMCU LED Control over WiFi Demo
+ *
+ * https://circuits4you.com
+ */
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+//ESP Web Server Library to host a web page
+#include <ESP8266WebServer.h>
+
+//---------------------------------------------------------------
+//Our HTML webpage contents in program memory
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<h1>WiFi Relay Control: </h1><br>
+Ciclk to turn <a href="ledOn">LED ON</a><br>
+Ciclk to turn <a href="ledOff">LED OFF</a><br>
+Ciclk to turn <a href="buzzOff">Buzz OFF</a><br>
+clik to turn <a href="buzzon">Buzz ON</a><br>
+
+<hr>
+</center>
+
+</body>
+</html>
+)=====";
+//---------------------------------------------------------------
+//On board LED Connected to GPIO2
+#define LED 14  
+#define BUZZ 16
+
+//SSID and Password of your WiFi router
+const char* ssid = "Toby";
+const char* password = "1234567890";
+
+//Declare a global object variable from the ESP8266WebServer class.
+ESP8266WebServer server(80); //Server on port 80
+
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+void handleRoot() {
+ Serial.println("You called root page");
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
+}
+
+void handleLEDon() { 
+ Serial.println("LED on page");
+ digitalWrite(LED,LOW); //LED is connected in reverse
+ server.send(200, "text/html", "LED is ON"); //Send ADC value only to client ajax request
+}
+
+void handleLEDoff() { 
+ Serial.println("LED off page");
+ digitalWrite(LED,HIGH); //LED off
+ server.send(200, "text/html", "LED is OFF"); //Send ADC value only to client ajax request
+}
+
+void handlebuzzoff() { 
+ Serial.println("LED off page");
+ digitalWrite(D0,LOW); //LED off
+ digitalWrite(16,LOW); //LED off
+
+ server.send(200, "text/html", "LED is OFF"); //Send ADC value only to client ajax request
+}
+
+void handlebuzzon() { 
+ Serial.println("Buzz On Page");
+ digitalWrite(D0,HIGH); //LED off
+  digitalWrite(16,HIGH); //LED off
+
+ server.send(200, "text/html", "BUZZ is ON"); //Send ADC value only to client ajax request
+}
+//==============================================================
+//                  SETUP
+//==============================================================
+void setup(void){
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("");
+
+  //Onboard LED port Direction output
+  pinMode(LED,OUTPUT); 
+  pinMode(16, OUTPUT);
+  //Power on LED state off
+  digitalWrite(LED,HIGH);
+  
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+ 
+  server.on("/", handleRoot);      //Which routine to handle at root location. This is display page
+  server.on("/ledOn", handleLEDon); //as Per  <a href="ledOn">, Subroutine to be called
+  server.on("/ledOff", handleLEDoff);
+    server.on("/buzzon", handlebuzzon);
+
+    server.on("/buzzOff", handlebuzzoff);
+
+
+  server.begin();                  //Start server
+  Serial.println("HTTP server started");
+}
+//==============================================================
+//                     LOOP
+//==============================================================
+void loop(void){
+  server.handleClient();          //Handle client requests
+}
