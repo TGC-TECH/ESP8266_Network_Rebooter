ef4829e8ba3576e9dbc7cf46c50c0ed1fb519353
Toby Cowles <tobycowles1015@gmail.com>
2019-11-17 15:19:39 -0500

    Add files via upload

---
+++ nobuzzer.sh
@@ -0,0 +1,18 @@
+while [ 2 > 1 ]
+do
+
+if ping -c 1 www.google.com &> /dev/null
+then
+  echo 1
+  echo "network OK"
+
+else
+  echo 0
+  echo "reseting network"
+  curl IPHERE/ledOff #Reset Command
+  sleep 5m
+  fi
+
+echo "sleeping"
+sleep 2m 
+done
